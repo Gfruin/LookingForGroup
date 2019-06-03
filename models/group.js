ee4ad72a09f2d0bf32c445d13f25c45fef0952f3
Greg Fruin <gfruin@gmail.com>
2019-06-03 14:37:20 -0500

    first_version model Group complete

---
+++ models/group.js
@@ -0,0 +1,21 @@
+//require mongoose
+const mongoose = require('mongoose')
+
+//create the group schema
+const groupSchema = new mongoose.Schema({
+	gamemaster: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User"
+	},
+	playerCharacters: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User"
+	},
+	title: String,
+	description: String
+})
+//create the group model
+const Group = mongoose.model("Group", groupSchema)
+//export
+
+module.exports = Group;
