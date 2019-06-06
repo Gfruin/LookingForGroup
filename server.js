c7dc4aab44c83f6e487883c0a7feece21aee3f91
Greg Fruin <gfruin@gmail.com>
2019-06-06 15:21:47 -0500

    added cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
-
+const cors = require('cors')
 //will need dotenv require and PORT and API_KEY requires
 require('dotenv').config()
 const PORT = process.env.PORT
@@ -21,6 +21,13 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
 
+const corsOptions = {
+	origin: process.env.REACT_CLIENT_URL,
+	credentials: true,
+	optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 //require the controllers once made: auth/login controller and user controller (plus others)
 const authController = require('./controllers/authController');
