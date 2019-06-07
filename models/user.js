6fb9d6212df70560d6c56e252f54f711ed166f16
Greg Fruin <gfruin@gmail.com>
2019-06-07 17:59:27 -0500

    added default values to userSchema

--- models/user.js
+++ models/user.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose')
 
 //define the schema
 const userSchema = new mongoose.Schema({
-	gamemaster: Boolean,
-	playerCharacter: Boolean, //maybe take out? 
-	lookingForGroup: Boolean,
-	gameMaterials: String,
-	experience: Number,
-	description: String,
-	firstName: String,
-	lastName: String,
-	password: String,
+	gamemaster: {type: Boolean, default: false},
+	playerCharacter: {type: Boolean, default: false}, //maybe take out? 
+	lookingForGroup: {type: Boolean, default: false},
+	gameMaterials: {type: String, default: null},
+	experience: {type: Number, default: 0},
+	description: {type: String, default: null},
+	firstName: {type: String, default: null},
+	lastName: {type: String, default: null},
+	password: {type: String, default: null},
 	username: {
 		type: String,
 		required: true,
@@ -25,24 +25,24 @@ const userSchema = new mongoose.Schema({
 	// may need to change the structure of the location object data
 	// geoJson? 
 	location: {
-		address: String,
-		latitude: String,
-		longitude: String,
-		zipCode: String
+		address: {type: String, default: null},
+		latitude: {type: String, default: null},
+		longitude: {type: String, default: null},
+		zipCode: {type: String, default: null}
 	},
 	gamesystem: {
-		dnd5e: Boolean,
-		pathfinder: Boolean,
-		starfinder: Boolean,
-		dnd3_5: Boolean,
-		callOfCthulu: Boolean,
-		other: String
+		dnd5e: {type: Boolean, default: false},
+		pathfinder: {type: Boolean, default: false},
+		starfinder: {type: Boolean, default: false},
+		dnd3_5: {type: Boolean, default: false},
+		callOfCthulu: {type: Boolean, default: false},
+		other: {type: String, default: null}
 	},
 	gamestyle: {
-		roleplay: Boolean,
-		combat: Boolean,
-		dungeonCrawl: Boolean,
-		other: String
+		roleplay: {type: Boolean, default: false},
+		combat: {type: Boolean, default: false},
+		dungeonCrawl: {type: Boolean, default: false},
+		other: {type: String, default: null}
 	},
 	//maybe add group field of groups that the user is in
 
